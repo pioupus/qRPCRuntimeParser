d5dba056b6648acbd1a2a884257ae907c94afb59
tr <tr@crystal-photonics.com>
2016-09-07 16:13:59 +0200

    starting on implementing decoded parameters

--- project/src/rpcruntime_decoded_param.h
+++ project/src/rpcruntime_decoded_param.h
@@ -1,15 +1,32 @@
 #ifndef RPCRUNTIMEDECODEDPARAM_H
 #define RPCRUNTIMEDECODEDPARAM_H
 
-#include "rpcruntime_transfer.h"
+#include <cinttypes>
+#include <istream>
+#include <utility>
+#include <vector>
+
+class RPCRuntimeParameterDescription;
 
 /*
  * A RPCRuntimeDecodedParam represents a script-understandable parameter in an RPC-Function call
  */
 
-class RPCRuntimeDecodedParam{
-public:
-	RPCRuntimeDecodedParam();
+class RPCRuntimeDecodedParam {
+	public:
+	enum class Type { integer, enumeration, structure, array };
+	RPCRuntimeDecodedParam(const RPCRuntimeParameterDescription &param);
+	uint64_t as_uint() const;
+	int64_t as_int() const;
+	std::pair<int64_t, std::string> as_enum() const;
+	std::pair<uint64_t, std::string> as_uenum() const;
+	std::vector<std::string, RPCRuntimeDecodedParam> as_struct() const;
+	std::vector<RPCRuntimeDecodedParam> as_array() const;
+private:
+	const RPCRuntimeParameterDescription *param;
 };
+
+std::istream &operator>>(std::istream &is, RPCRuntimeDecodedParam &param);
+std::ostream &operator<<(std::ostream &os, const RPCRuntimeDecodedParam &param);
 
 #endif //RPCRUNTIMEDECODEDPARAM_H
